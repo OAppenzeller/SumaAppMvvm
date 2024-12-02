60c4a3bd65ac066e90e8624a5211cb621c0c0bdb
OAppenzeller <o.appenzeller@yahoo.com>
2024-12-01 21:40:49 -0600

    Add project files.

---
+++ SumaAppMvvm/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SumaAppMvvm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(root: new Views.MainPage());
+        }
+    }
+}
---
+++ SumaAppMvvm/ViewModels/SumaViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SumaAppMvvm.ViewModels
+{
+    internal class SumaViewModel: INotifyPropertyChanged
+    {
+        private string _value1;
+        private string _value2;
+        private string _result;
+
+        public string Value1
+        {
+            get => _value1;
+            set
+            {
+                _value1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value2
+        {
+            get => _value2;
+            set
+            {
+                _value2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Result
+        {
+            get => _result;
+            set
+            {
+                _result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CalculateCommand { get; }
+        public ICommand ClearCommand { get; }
+
+        public SumaViewModel()
+        {
+            CalculateCommand = new Command(CalculateSum);
+            ClearCommand = new Command(ClearFields);
+        }
+
+        private void CalculateSum()
+        {
+            if (string.IsNullOrWhiteSpace(Value1) || string.IsNullOrWhiteSpace(Value2))
+            {
+                Result = "Error: Todos los campos son obligatorios.";
+                return;
+            }
+
+            if (double.TryParse(Value1, out double num1) && double.TryParse(Value2, out double num2))
+            {
+                Result = $"Resultado: {num1 + num2}";
+            }
+            else
+            {
+                Result = "Error: Solo se permiten valores numéricos.";
+            }
+        }
+
+        private void ClearFields()
+        {
+            Value1 = string.Empty;
+            Value2 = string.Empty;
+            Result = string.Empty;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
